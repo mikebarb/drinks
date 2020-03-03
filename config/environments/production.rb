b516ea5cfe3ade073409a159a55c88b169cd1515
micmac <mike@mikebarb.net>
2020-03-03 07:13:46 +0000

    Update parameters for production redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,10 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.web_socket_server_url = "wss://whispering-fjord-76076.herokuapp.com/cable"
+
+  # Action cable can only accept WebSocket requests from specified origins.
+  config.action_cable.allowed_request_origins = ['https://whiispering-fjord-76076.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
